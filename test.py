70921fa9b2efc93bb26c9c125321b9b5e817349e
avivabalata <avevanes@post.bgu.ac.il>
2018-12-25 20:33:33 +0200

    25.12 first part

--- test.py
+++ test.py
@@ -4,10 +4,9 @@ import numpy
 from os import listdir
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import PorterStemmer
+import matplotlib.pyplot as plt
 
 path = "D:\\documents\\users\\avevanes\\Downloads\\ohsumed-first-20000-docs"
-
-infoTable = []
 
 # declaration
 tokenize = []
@@ -20,10 +19,13 @@ testFileDirs = []
 trainData = []
 testData = []
 
+infoTable = []
 
-# categoties = []
+categoties = []
 docForCategory = []
 termsForCategory = []
+
+
 from nltk.corpus import stopwords
 
 stop_words = stopwords.words('english')
@@ -32,11 +34,9 @@ def cleanAndNormalizeText(data):
 
     # tokenization
     tokenize = word_tokenize(data)
-
     # remove stop words
     filterText = [w for w in tokenize if not w in stop_words]
     filterText = [w for w in filterText if not len(w) <= 1]
-
     # stem
     ps = PorterStemmer()
     for i in range(len(filterText)-1):
@@ -45,9 +45,7 @@ def cleanAndNormalizeText(data):
                 filterText[i] = ps.stem(filterText[i])
             except Exception as e:
                 filterText[i] = filterText[i]
-
-    return ''.join(filterText)
-
+    return ' '.join(filterText)
 
 trainFolder = os.listdir(path+ '\\training')
 testFolder = os.listdir(path+ '\\test')
@@ -58,7 +56,7 @@ for folder in trainFolder:
         text = fileReader.read().lower()
         fileReader.close()
         Text = cleanAndNormalizeText(text)
-        infoTable.append(['train',folder, file, Text])
+        infoTable.append(['train',folder,Text, file])
 for folder in testFolder:
     testFileDirs = os.listdir(path+ '\\test\\'+ folder)
     for file in testFileDirs:
@@ -66,12 +64,30 @@ for folder in testFolder:
         text = fileReader.read().lower()
         fileReader.close()
         Text = cleanAndNormalizeText(text)
-        infoTable.append(['test',folder, file, Text])
+        # infoTable.append(['test',folder, file, Text])
 
 
-table = pd.DataFrame(infoTable, columns=['type','category','text','file'])
-print('Number of categories', numpy.unique(table['category'].size))
+trainTable = pd.DataFrame(infoTable, columns=['type','category','text','file'])
 
+# term distribution per category
+print('Number of categories', numpy.unique (trainTable['category']).size)
 
-docForCategory = pd.groupby(by='category', as_index=False).agg({'file': pd.Series.nunique})
-print(docForCategory)
+# category and num of docs in it
+docForCategory = trainTable.groupby(by='category', as_index=False).agg({'file': pd.Series.nunique})
+docForCategory
+
+# term distribution per category
+categories = trainTable['category']
+for cat in categories:
+    categoryFiles = trainTable.loc[trainTable['category'] == cat]
+    categoryData = [" ".join(set(txt.split(' '))) for txt in categoryFiles['text']]
+    categoryData = " ".join(categoryData)
+    termsCount = pd.Series(categoryData.split()).value_counts()
+    categoryTermValues = pd.DataFrame
+    categoryTermValues['term'] = termsCount.index
+    categoryTermValues['count'] = list(termsCount)
+    setCount=set(categoryTermValues['count'])
+    his = categoryTermValues.hist(columns='count',bins=len(setCount))
+    plt.xlabel("number of files")
+    plt.ylabel("number of terms")
+    plt.show(his)
